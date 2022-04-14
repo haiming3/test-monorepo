8d0c49c43a715f5d638aee2dea2029c0420f8d06
haiming <haiming@cicada-speech.com>
2022-04-14 15:48:38 +0800

    12312

---
+++ commitlint.config.js
@@ -0,0 +1,32 @@
+/**
+* major: A code change that can upgrade the major version
+* feat: A new feature
+* data: Data update
+* fix: A bug fix
+* enh: Code enhancement
+* refactor: A code change that neither fixes a bug nor adds a feature
+* perf: A code change that improves performance
+* chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+* test: Adding missing or correcting existing tests
+* docs: Documentation only changes
+*/
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', 'data', 'fix', 'enh', 'refactor', 'perf', 'chore', 'test', 'docs', 'major'
+    ]],
+    'type-empty': [2, 'never'],
+    'type-case': [0],
+    'scope-enum': [2, 'always', [
+      'common', 'all', 'zoom', 'wechat', 'hisense', 'windquiet', 'triage', 'ucrest', 'coinbase', 'chime', 'new'
+    ]],
+    'scope-empty': [2, 'never'],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
+  }
+}
